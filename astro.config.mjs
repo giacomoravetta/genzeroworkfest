4d2e0e4131927544dc1d94318dc0665d8723d62e
Giacomo Ravetta <102873147+giacomoravetta@users.noreply.github.com>
2025-10-01 19:35:27 +0200

    Disable prerendering for Info component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://genzeroworkfest.it",
+  output: "server",
+  adapter: cloudflare(),
   integrations: [svelte()],
   vite: {
     plugins: [tailwindcss()],
